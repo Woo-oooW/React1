0014027fabade31d0b1b56f749db2620a115cae0
Woo <swy831@gmail.com>
2024-07-13 18:55:42 +0900

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   react-nomad/package-lock.json
    	modified:   react-nomad/package.json
    	modified:   react-nomad/src/App.js
    	new file:   react-nomad/src/App.module.css
    	modified:   react-nomad/src/Button.js
    	new file:   react-nomad/src/Button.module.css

--- react-nomad/src/App.js
+++ react-nomad/src/App.js
@@ -1,9 +1,17 @@
+import Button from "./Button";
+import PropTypes from "prop-types";
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div>
-      <h1>Welcome back!!!!!!!!!!!!!!!!</h1>
+      <h1 className={styles.title}>Welcome back!!!asdasdas!!!!!!!!!!!!!</h1>
+      <Button text={"hihihihihihi"}/>
     </div>
   );
+}
+Button.prototype = {
+  text : PropTypes.string.isRequired
 }
 
 export default App;
--- react-nomad/src/Button.js
+++ react-nomad/src/Button.js
@@ -1,4 +1,11 @@
-function Button({ text }) {
-  return <button>{text}</button>;
-}
+import PropTypes from "prop-types";
+import styles from "./Button.module.css";
+
+function Button({ text }) 
+{
+  return  (
+<button className ={styles.btn}>
+    {text}
+</button>
+)}
 export default Button;
